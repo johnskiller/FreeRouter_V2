b81a2ddceaa3b9537c554d91e5a7745d4126a53c
johnskiller <john@skykiller.com>
2015-04-06 17:03:35 +0800

    only change ip rule when pptp-linode is up/down

--- OpenWRT/etc/ppp/ip-down.d/vpndown.sh
+++ OpenWRT/etc/ppp/ip-down.d/vpndown.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 LOG='/tmp/vpn.log'
+if [ $1 == "pptp-linode" ]; then
 echo "\$Log: VPN Disconnect! @$(date +"%T@%Y-%m-%d")" >>$LOG
 echo "\$Log: Remove IP rules for table 'vpn'! @$(date +"%T@%Y-%m-%d")" >>$LOG
 ip rule del table vpn
+ip rule del from all fwmark 1
+# rules for apple tv
+ip rule del from 192.168.31.25
+fi
--- OpenWRT/etc/ppp/ip-up.d/vpnup.sh
+++ OpenWRT/etc/ppp/ip-up.d/vpnup.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
+# Script which handles the routing issues as necessary for pppd
+# Only the link to Newman requires this handling.
+#
+# When the ppp link comes up, this script is called with the following
+# parameters
+#       $1      the interface name used by pppd (e.g. ppp3)
+#       $2      the tty device name
+#       $3      the tty device speed
+#       $4      the local IP address for the interface
+#       $5      the remote IP address
+#       $6      the parameter specified by the 'ipparam' option to pppd
+#
 set -x
 LOG='/tmp/vpn.log'
+echo "ip-up $1,$2,$3,$4,$5,$6" >> $LOG
+if [ $1 == "pppoe-wan" ]; then
+	echo "WAN UP @$(date +"%T@%Y-%m-%d")" >>$LOG
+fi
+
+if [ $1 == "pptp-linode" ]; then
 echo "\$Log: VPN Connected! @$(date +"%T@%Y-%m-%d")" >>$LOG
 VPN_DEV=$(ifconfig | grep "pptp" | sed -e "s#^\([^ ]*\) .*#\1#g")
 echo "\$Log: Add DNS servers to VPN route! @$(date +"%T@%Y-%m-%d")" >>$LOG
@@ -9,4 +27,7 @@ echo "\$Log: Add VPN device for table 'vpn'! @$(date +"%T@%Y-%m-%d")" >>$LOG
 ip route add default dev $VPN_DEV table vpn 
 echo "\$Log: Add IP marked by firewall to table 'vpn'! @$(date +"%T@%Y-%m-%d")" >>$LOG  
 ip rule add fwmark 1 priority 1984 table vpn
+# rule for apple tv, allways to vpn
+ip rule add from 192.168.31.25 lookup vpn
 /etc/init.d/dnsmasq restart
+fi
